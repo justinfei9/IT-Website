d2a9f7ebbfcaf565d934c5455fcb91c0730dd3e7
Justin <jpf99@yahoo.com>
2025-12-06 21:22:12 -0500

    fix pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/IT-Website/', 
       
       server: {
         port: 3000,
